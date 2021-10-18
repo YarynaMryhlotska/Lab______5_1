49425f47c9dbcf03a6d7e5e561cd855e31d0d6f2
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-10-19 00:30:45 +0300

    1

---
+++ Test___Laba_5_1/Test___Laba_5_1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestLaba51
+{
+	TEST_CLASS(TestLaba51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
